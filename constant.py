47bcd9b07d93ece187f280aff3c43a267b61a1c7
peter abel <peterabel791@gmail.com>
2022-10-14 19:46:30 +0300

    final
    Signed-off-by: peter abel <peterabel791@gmail.com>

--- constant.py
+++ constant.py
@@ -2,7 +2,7 @@
 '''Kaprekar constant, or 6174, is a constant that arises when we take a 4-digit integer,
  form the largest and smallest numbers from its digits, and then subtract these two numbers.
  Continuing with this process of forming and subtracting, 
- we will always arrive at the number 6174.'''
+ we will always arrive at the number 6174. The only condition is that atleast two digits in the number  be different'''
 
 
 
@@ -16,17 +16,15 @@ mylist = []
 def generating_numbers():
     for i in range(1000,9999) :
         mylist.append(i)
-
+    #The condition that requires atleast two numbers in the integer to be different
     for i in mylist:
       if i == 1111 or i == 2222 or i== 3333 or i == 4444 or i == 5555 or i == 6666 or i == 7777 or i == 8888 or i == 9999:
          mylist.remove(i)
     return mylist
 
-#print(len(generating_numbers()))
 
 
-
-#function to reorganize number to descending order
+#function to reorganize number to descending order eg. (5273->7532)
 def largest_number(num):
     num = str(num)
     ordered = ''.join(sorted(num, reverse=True))
@@ -34,7 +32,7 @@ def largest_number(num):
 
 
 
-#function to reorganize number to ascending order
+#function to reorganize number to ascending order eg.(5273->2357)
 def small_number(num):
     num = str(num)
     ordered = ''.join(sorted(num, reverse=True))
@@ -56,4 +54,4 @@ def subtract(i):
         
 '''The range can be adjusted to check all other numbers in the list'''
 for i in range (1,10):
-    print(subtract(i))
+    print('Kaprekars number:',subtract(i))
