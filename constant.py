0cf5ab05b912b4dd2c22cf18de3bc58e4d26fde0
peter abel <peterabel791@gmail.com>
2022-10-14 19:35:14 +0300

    adding doc string
    Signed-off-by: peter abel <peterabel791@gmail.com>

--- constant.py
+++ constant.py
@@ -1,12 +1,16 @@
 
-#Kaprekars constant 6174
+'''Kaprekar constant, or 6174, is a constant that arises when we take a 4-digit integer,
+ form the largest and smallest numbers from its digits, and then subtract these two numbers.
+ Continuing with this process of forming and subtracting, 
+ we will always arrive at the number 6174.'''
 
 
-#import random
 
 
-
+#Our four digit integer range
 nums = range(1000,9999)
+
+#creating an empty list
 mylist = []
 #function to generate the numbers.
 def generating_numbers():
