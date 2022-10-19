983b1a0be72c598f597ea4b146188a979262524c
Abel Peter <peterabel791@gmail.com>
2022-10-19 14:51:26 +0300

    test 2
    Signed-off-by: Abel Peter <peterabel791@gmail.com>

--- test_constant.py
+++ test_constant.py
@@ -1,11 +1,18 @@
 import unittest
 
-from constant import generating_numbers, mylist
+from constant import generating_numbers, mylist, largest_number
 
 class TestConstant(unittest.TestCase):
     def test_list_int(self):
         generating_numbers()
         self.assertTrue(len(mylist), 8991)
+        
+
+class Test2(unittest.TestCase):
+    def test_list_2(self):
+        y = 7325
+        x = largest_number(5273)
+        self.assertCountEqual(str(x),str(y))
         
 
 
