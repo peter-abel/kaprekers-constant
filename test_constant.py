450808a8a9537cbe350f56688a6fcf6e1f264571
Abel Peter <peterabel791@gmail.com>
2022-10-19 14:52:57 +0300

    test 3
    Signed-off-by: Abel Peter <peterabel791@gmail.com>

--- test_constant.py
+++ test_constant.py
@@ -1,6 +1,6 @@
 import unittest
 
-from constant import generating_numbers, mylist, largest_number
+from constant import generating_numbers, mylist, largest_number, small_number
 
 class TestConstant(unittest.TestCase):
     def test_list_int(self):
@@ -13,6 +13,12 @@ class Test2(unittest.TestCase):
         y = 7325
         x = largest_number(5273)
         self.assertCountEqual(str(x),str(y))
+
+class Test3(unittest.TestCase):
+    def test_list_3(self):
+        y = 3142
+        x = small_number(1234)
+        self.assertCountEqual(str(x),str(y))        
         
 
 
